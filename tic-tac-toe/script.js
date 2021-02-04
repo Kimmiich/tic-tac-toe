a1bd5f989df720c7c42acc624762276d015beb55
Kimmiich <kimmielundgren@live.se>
2021-02-04 20:39:32 +0100

    added style

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -22,15 +22,13 @@ board.addEventListener('click', function (event) {
   
   if (document.getElementById(event.target.id).innerHTML === '') {
     if (player%2) {
-      document.getElementById(event.target.id).innerHTML = 'X';
-      playertag.innerHTML = 'player O turn';
+      document.getElementById(event.target.id).innerHTML = `<span>X</span>`;
+      playertag.innerHTML = 'Player O turn';
       playerX.push(event.target.id);
-      console.log(playerX)
     } else {
-      document.getElementById(event.target.id).innerHTML = 'O'
-      playertag.innerHTML = 'player X turn';
+      document.getElementById(event.target.id).innerHTML = `<span>O</span>`
+      playertag.innerHTML = 'Player X turn';
       playerO.push(event.target.id);
-      console.log(playerO)
     }
     player++;
   }
@@ -42,12 +40,11 @@ const checkWinner = () => {
   if (playerX.length > 2) {
     winningCond.forEach(condition => { 
       if (playerX.includes(condition[0].toString()) && playerX.includes(condition[1].toString()) && playerX.includes(condition[2].toString())) {
-        console.log('player x won')
-        return playertag.innerHTML = 'player X wins!'
+        return playertag.innerHTML = 'Player X wins!'
       } else if (playerO.includes(condition[0].toString()) && playerO.includes(condition[1].toString()) && playerO.includes(condition[2].toString())) {
-        return playertag.innerHTML =  'player O wins!'
+        return playertag.innerHTML =  'Player O wins!'
       } else if (!playerX.includes(condition[0].toString()) && !playerX.includes(condition[1].toString()) && !playerX.includes(condition[2].toString()) && playerX.length === 5) {
-        return playertag.innerHTML = "it's a tie, restart the game!"
+        return playertag.innerHTML = "It's a tie, restart the game!"
       }
     })
   }
@@ -56,17 +53,4 @@ const checkWinner = () => {
 restart.addEventListener('click', function () {
   location.reload();
 })
-
-
-// document.getElementById('result').innerHTML = `${winningPlayer} won!!!!`;
-
-
-
-  // if (document.getElementById('1').innerHTML === 'X' && document.getElementById('2').innerHTML === 'X' && document.getElementById('3').innerHTML === 'X') {
-  //   console.log('yeeey')
-  // } else if (document.getElementById('4').innerHTML === 'X' && document.getElementById('2').innerHTML === 'X' && document.getElementById('3').innerHTML === 'X') {
-  //   console.log()
-  // } else if (document.getElementById('1').innerHTML === 'X' && document.getElementById('2').innerHTML === 'X' && document.getElementById('3').innerHTML === 'X') { }
-
-
 
